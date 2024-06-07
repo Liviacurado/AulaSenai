e69593d237104072627c274ac66f851ff77d0993
Liviacurado <curadolivia@gmail.com>
2024-06-07 17:32:33 -0300

    Update App.java
    ESTUDO DE VETORES /MATRIZ

--- src/App.java
+++ src/App.java
@@ -41,34 +41,83 @@ public class App {
 //     } 
      
      
-    int i,j,posi,t,i2;
+//     int i,j,posi,t,i2;
 
-    posi= 6;
-    t =1;
+//     posi= 6;
+//     t =1;
 
-    for ( i=1 ; i<=6; i ++) {
-       for ( j=1 ; j<=10 ; j++ )
-         if (j==posi) {     
-            for (i2=1; i2 <= t; i2 ++) {
-                System.out.print('*');
-            }
-            t+=2;
-              posi--;
+//     for ( i=1 ; i<=6; i ++) {
+//        for ( j=1 ; j<=10 ; j++ )
+//          if (j==posi) {     
+//             for (i2=1; i2 <= t; i2 ++) {
+//                 System.out.print('*');
+//             }
+//             t+=2;
+//               posi--;
 
             
 
-        }
-          else 
-            System.out.print(' ');
+//         }
+//           else 
+//             System.out.print(' ');
            
-               System.out.println();
+//                System.out.println();
 
 
-         }
+//          }
 
         
             
-    }
+//     }
         
 
+// }
+
+ // VETORES : Sessão de espaços ( um do lado do outro)
+
+           // MATRIZ: vetores organizados em linha ( horizontal e vertical )
+           // i = linha  0 a 4
+           // j = coluna 0 a 4 
+
+                 // ----------VETOR------------
+            // int tam = 10;
+            // int vetor  [] = new int[tam];
+            // int i;
+
+            //     for ( i=1;i<tam;i++){
+
+            //         vetor [i] = 10;
+                    
+            //     }
+            //        for ( i=1; i<tam;i++) {
+                    
+            //         System.out.print(vetor [i]+ " ");}
+
+                   //--------- MATRIZ -------
+
+                   int tam =  10 ; 
+                   int matriz [] [] = new int [tam] [tam];
+                   int i,j;
+                    for (i=0;i<tam;i++){
+                       for (j=0;j<tam ; j ++){
+                        matriz [i][j]=i*j;
+                       }
+                       
+                    }
+                    for (i=0;i<tam;i++){
+                        for (j=0;j<tam ; j ++){
+                         matriz [i][j]=i*j;
+                         System.out.print(matriz [i][j]+" ");
+                        }
+
+                        System.out.println();
+                        
+                     }
+
+                    
+
+        
+        
+
+    }
 }
