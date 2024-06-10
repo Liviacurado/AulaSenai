9e944ec708923df76b81b0eac9ee1ac570988a54
Liviacurado <curadolivia@gmail.com>
2024-06-10 17:36:51 -0300

    Estudo de variáveis e condicionais
    Estudo de variáveis e condicionais

---
+++ Aula02/App.java
@@ -0,0 +1,62 @@
+public class App {
+    public static void main(String[] args)  {
+        
+        // int NUM = 34;
+        // float decimal = 1.0f;
+        // double dec = 3.5;
+        // char unico = 'a';
+        // String texto = "Text";
+        // boolean teste = true;
+        // byte meuByte = 00110;
+        // long numeroGrande = 99999999;
+        
+        // int num1 = 25;
+        // int num2 = 33;
+        // int num3 = 28;
+        // int resultado;
+        // resultado = num1 + num2 + num3;
+        //   System.out.println("O resultado é: "+ resultado);
+
+
+        //   String Nome  = "Livia de Jesus"
+        //   System.out.println(Nome);
+
+//         int idade = 17; 
+
+//         if ( idade == 18){
+//             System.out.println("Você é maior de idade");
+//         }
+//            else if (idade <=18){
+            
+//             System.out.println("Você é menor de idade");
+          
+        
+//                       }
+
+//        else {
+//             System.out.println("Você é maior de idade");
+//                       }
+    
+
+
+//     }
+
+
+
+// int num1 = 3;
+// int num2 = 9; 
+// int resultado = (num1 * num2); 
+
+// if ( resultado <=0 )
+//       {System.out.println("O "+resultado+" é negativo ");
+// }  
+
+// else
+//  {System.out.println("O "+ resultado +" é positivo");
+// }
+//       }
+// } 
+
+
+
+
