58ee15df20f84ff8ab3c7a34403dadb3be926fb0
Liviacurado <curadolivia@gmail.com>
2024-06-14 17:09:22 -0300

    Update Empresa.java

--- Empresa/src/Empresa.java
+++ Empresa/src/Empresa.java
@@ -1,5 +1,48 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Empresa
+
+{
+    public static void main(String[] args) {
+        
     }
-}
+    private String Funcionário; 
+    private String nome;
+    private String cpf;
+    private double valorVenda;
+
+    public String getFuncionário() {
+        return Funcionário;
+    }
+    public void setFuncionário(String funcionário) {
+        Funcionário = funcionário;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+            public double venda (Double novaVenda){
+
+                novaVenda += valorVenda;
+
+                return valorVenda;
+
+
+            }
+             
+            public double bonific () {
+                
+                    return valorVenda * 2.0;
+
+                
+            
+        
+            }
+        }
