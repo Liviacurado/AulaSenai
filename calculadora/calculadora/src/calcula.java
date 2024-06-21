1f39b724d3f2f93e3369daf7f667f7cb2b3693ab
Liviacurado <curadolivia@gmail.com>
2024-06-21 17:02:53 -0300

    interface calculadora incompleto

--- calculadora/calculadora/src/calcula.java
+++ calculadora/calculadora/src/calcula.java
@@ -1,36 +1,37 @@
 
-
-import java.util.Scanner;
-
+import javax.swing.*;
 public class calcula {
     
         public static void main(String[] args) throws Exception {
-           
-            Scanner entrada = new Scanner (System.in);
+
+
+
             int primeiro=0,segundo=0;
             double resultado;
     
             double resDiv;
     
-    
+                 int opcao=-1;
             // args = argumentos 
-            int opcao = -1;
-    
-                while (opcao!=0){
-    
-                    System.out.println("Insira uma opção: ");
-                    opcao = entrada.nextInt();
-                            
-                    if ( opcao !=0){
-                     System.out.println("Insira o primeiro número: ");
-                        primeiro = entrada.nextInt();
-    
-                       System.out.println("Inserir o segundo número: ");
-                          segundo = entrada.nextInt();
-                    }
-    
+            JTextField primeiroField = new JTextField(5);
+            JTextField segundoField = new JTextField(5);
+            
+                JPanel cal = new JPanel();
+                  cal.add(new JLabel("Primeiro número"));
+                   cal.add(primeiroField);
+                    cal.add(Box.createHorizontalStrut(5)); // a spacer
+                    cal.add(new JLabel("Segundo número"));
+                    cal.add(segundoField);
+
+                    int result = JOptionPane.showConfirmDialog(null, cal, 
+                    "Coloque os valores", JOptionPane.OK_CANCEL_OPTION);
+                    if (result == JOptionPane.OK_OPTION) {
+                  System.out.println("Primeiro " + primeiroField.getText());
+                  System.out.println("Segundo" + primeiroField.getText());
+      }
+
                 switch (opcao) {
-                    case 1 -> {
+                    case 1:  {
                         // soma
                         resultado = primeiro+segundo;
                         System.out.println("o resultado é: " + resultado+"\n");
@@ -38,21 +39,21 @@ public class calcula {
     
                     }
     
-                    case 2 -> {
+                    case 2: {
                         //subtração 
                         resultado=primeiro-segundo;
                         System.out.println("o resultado é: "+resultado+"\n");
                         break;
                     }
     
-                    case 3 -> {
+                    case 3 : {
                         resultado=primeiro*segundo;
                         System.out.println("o resultado é: " + resultado+"\n");
                         break;
                         // multiplicação
                     }
     
-                    case 4 -> {
+                    case 4: {
                         //Divisão 
                         resDiv=(double)primeiro/(double)segundo;
                         System.out.println("o resultado é: "+resDiv+"\n");
@@ -60,25 +61,26 @@ public class calcula {
     
                     }
     
-                    case 5 -> {
+                    case 5 :{
                         resultado = Math.pow(primeiro,segundo);
     
                         System.out.println(resultado);
                         break;
                     }
                         
-                          case 0 ->{ System.out.println("Encerrando!");
+                          case 0 :{ System.out.println("Encerrando!");
                         break;
                         }
     
                 
-                    default -> System.out.println("Error");
+                    default:
+                     System.out.println("Error");
                      }
                 }
     
             
             
         }
-    }
+    
     
 
