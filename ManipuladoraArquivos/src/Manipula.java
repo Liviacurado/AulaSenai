2305ef519abb86e44c4a5334356a42d375cbb5f7
Liviacurado <curadolivia@gmail.com>
2024-06-26 16:07:36 -0300

    parte do código

--- ManipuladoraArquivos/src/Manipula.java
+++ ManipuladoraArquivos/src/Manipula.java
@@ -3,7 +3,9 @@ public class Manipula {
 public static void main(String[] args) throws Exception {
 
     String caminho = "ManipulaArquivo\\teste.txt";
-     
+
+    ManipuladoraArquivos.escrita(caminho);
+     ManipuladoraArquivos.leitura(caminho);
    
 }
 }
