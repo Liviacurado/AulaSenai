d340246e16e8dd5b7228ef64a95dc5c0355c26d8
Liviacurado <curadolivia@gmail.com>
2024-07-30 11:18:03 -0300

    ADICIONANDO O MENU PRINCIPAL

--- Estudos antes/estudosseparados/src/App.java
+++ Estudos antes/estudosseparados/src/App.java
@@ -5,6 +5,39 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) throws Exception {
 
+      String[] options = new String[] { "Cadastrar Despesa", "Abastecer Veículo", "Mostrar Despesas Cadastradas", "Voltar" };
+            int escolha = JOptionPane.showOptionDialog
+
+        //     int escolha = JOptionPane.showOptionDialog(
+        //             null,
+        //             "Selecione",
+        //             "Gestão de Gastos",
+        //             JOptionPane.DEFAULT_OPTION,
+        //             JOptionPane.PLAIN_MESSAGE,
+        //             null,
+        //             options,
+        //             options[0]);
+
+        //     switch (escolha) {
+        //         case 0:
+        //             gasto.cadastrarGasto(veiculo);
+        //             break;
+        //         case 1:
+        //             gasto.abastecerVeiculo(veiculo);
+        //             break;
+        //         case 2:
+        //             gasto.mostrarGastos();
+        //             break;
+        //         case 3:
+        //             exibirMenu();
+        //             break;
+        //         default:
+        //             throw new AssertionError();
+        //     }
+        // }
+    
+
+
         JFrame jf= new JFrame();
           jf.setLayout(new BorderLayout(10,5));
           jf.setTitle("Turistando");
