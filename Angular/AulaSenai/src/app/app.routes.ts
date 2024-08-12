3324187ee21a72bcda3fcb975ff29f5d59fbe75a
Liviacurado <curadolivia@gmail.com>
2024-08-12 16:30:01 -0300

    abas de listar veiculo e  cadastrar despesas criado

--- Angular/AulaSenai/src/app/app.routes.ts
+++ Angular/AulaSenai/src/app/app.routes.ts
@@ -3,11 +3,15 @@ import {Routes,RouterModule}from'@angular/router';
 import { MenuComponent } from './pages/menu/menu.component';
 import { CadastarveiculoComponent } from './pages/cadastarveiculo/cadastarveiculo.component';
 import { CadastrarAbastecimentoComponent } from './pages/cadastrar-abastecimento/cadastrar-abastecimento.component';
+import { ListarveiculosComponent } from './pages/listarveiculos/listarveiculos.component';
+import { CadastrardespesaComponent } from './pages/cadastrardespesa/cadastrardespesa.component';
 
 export const routes: Routes = [
    { path: '', component: MenuComponent},
    { path:'cadastarveiculo',component:CadastarveiculoComponent},
-   {path:'cadastrarabastecimento',component:CadastrarAbastecimentoComponent}
+   {path:'cadastrarabastecimento',component:CadastrarAbastecimentoComponent},
+   {path:'listarveiculos',component:ListarveiculosComponent},
+   {path:'cadastrardespesa',component:CadastrardespesaComponent}
 ];
 @NgModule({
 imports:[RouterModule.forRoot (routes)],
