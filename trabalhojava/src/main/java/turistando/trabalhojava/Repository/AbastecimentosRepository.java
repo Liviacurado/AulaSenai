bfe9ae43a56323a5b4c3017dceac0bd0325e671f
Liviacurado <curadolivia@gmail.com>
2024-08-23 16:26:32 -0300

    ok

--- trabalhojava/src/main/java/turistando/trabalhojava/Repository/AbastecimentosRepository.java
+++ trabalhojava/src/main/java/turistando/trabalhojava/Repository/AbastecimentosRepository.java
@@ -3,10 +3,11 @@ package turistando.trabalhojava.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import turistando.trabalhojava.Model.AbastecimentoModel;
 
-
+@Repository
 public interface AbastecimentosRepository extends JpaRepository <AbastecimentoModel, String >{
 
     public List<AbastecimentoModel> findAll();
